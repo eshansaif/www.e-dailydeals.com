98599ceaba93a39e3b3dc1aaabe4c81c3137a2a4
unknown <eshansaif1234@gmail.com>
2019-09-20 01:07:40 +0600

    Frontend Tempalting id done

---
+++ resources/views/layouts/frontend/_head.blade.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>Porto - Bootstrap eCommerce Template</title>
+
+<meta name="keywords" content="HTML5 Template" />
+<meta name="description" content="Porto - Bootstrap eCommerce Template">
+<meta name="author" content="SW-THEMES">
+
+<!-- Favicon -->
+<link rel="icon" type="image/x-icon" href="{{ asset('assets/frontend/assets/images/icons/favicon.ico') }}">
+
+<!-- Plugins CSS File -->
+<link rel="stylesheet" href="{{ asset('assets/frontend/assets/css/bootstrap.min.css') }}">
+
+<!-- Main CSS File -->
+<link rel="stylesheet" href="{{ asset('assets/frontend/assets/css/style.min.css') }}">
---
+++ resources/views/layouts/frontend/_scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('assets/frontend/assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/assets/js/plugins.min.js') }}"></script>
+
+<!-- Main JS File -->
+<script src="{{ asset('assets/frontend/assets/js/main.min.js') }}"></script>
